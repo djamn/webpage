cd96ad9c89f362185c122a2237e79798c57fe002
daadii <dadi1@speed.at>
2024-07-22 12:09:01 +0200

    added firebaseConfig

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCdjdSNH1KjIQA2Gz-83UeBFHfEwrxnzA4",
+        authDomain: "djamn-webpage.firebaseapp.com",
+        databaseURL: "https://djamn-webpage-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "djamn-webpage",
+        storageBucket: "djamn-webpage.appspot.com",
+        messagingSenderId: "531743368185",
+        appId: "1:531743368185:web:30d7a38fecd2e123f3d328",
+        measurementId: "G-Z4B0F3SLC0"
+      }
+};

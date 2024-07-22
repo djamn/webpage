f2bfec0f5eea65d3374a4fe490ca97cc63ed9eaa
daadii <dadi1@speed.at>
2024-07-22 13:00:05 +0200

    added firestore basic commands

--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {collection, collectionData, deleteDoc, doc, Firestore, setDoc, updateDoc} from "@angular/fire/firestore";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) { }
+
+  getCollection(collectionName: string): Observable<any[]> {
+    const collectionRef = collection(this.firestore, collectionName);
+    return collectionData(collectionRef);
+  }
+
+  addDocument(collectionName: string, data: any): Promise<void> {
+    const collectionRef = collection(this.firestore, collectionName);
+    const docRef = doc(collectionRef);
+    return setDoc(docRef, data);
+  }
+
+  updateDocument(collectionName: string, docId: string, data: any): Promise<void> {
+    const docRef = doc(this.firestore, `${collectionName}/${docId}`);
+    return updateDoc(docRef, data);
+  }
+
+  deleteDocument(collectionName: string, docId: string): Promise<void> {
+    const docRef = doc(this.firestore, `${collectionName}/${docId}`);
+    return deleteDoc(docRef);
+  }
 }

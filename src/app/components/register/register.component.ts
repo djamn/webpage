6b4e84b15e50b84581100135d5f3a9aebdfb363b
daadii <dadi1@speed.at>
2024-07-23 21:59:03 +0200

    implemented first working version of register layout

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, signal} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {merge} from "rxjs";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'register-component',
@@ -9,7 +13,43 @@ export class RegisterComponent {
   @Input()
   imageSrc: string = "/assets/no-image.svg";
 
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  errorMessage = signal('');
+
+  @Input()
+  registerInputPlaceholderPassword: string = 'Enter your password'
+
+  @Input()
+  registerInputPlaceholderEmail: string = 'Enter your email'
+
+  @Input()
+  registerInputPlaceholderUsername: string = 'Enter your username'
+
+  hide = signal(true);
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  constructor(private authService: AuthService) {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
   register() {
     console.log("Hallo")
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      console.log("??")
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
   }
 }

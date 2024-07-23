77b804c81100b4fd49256f28968f6c27dffe8b8f
daadii <dadi1@speed.at>
2024-07-23 19:53:39 +0200

    added some global styles and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {MainPage} from "./pages/mainpage/mainpage.component";
+import {Register} from "./pages/register/register.component";
+import {Login} from "./pages/login/login.component";
+import {ResetPasswordComponent} from "./components/reset-password/reset-password.component";
 
 export const routes: Routes = [
   {path: '', component: MainPage, pathMatch: 'full'},
-
+  {path: 'register', component: Register},
+  {path: 'login', component: Login},
+  {path: 'reset-password', component: ResetPasswordComponent}
 
 ];

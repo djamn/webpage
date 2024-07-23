888c8bf27d79624f6f0d5038ad734a34b0e0a372
daadii <dadi1@speed.at>
2024-07-23 19:39:15 +0200

    added login/register component structure

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,18 +7,30 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MaterialModule} from "../material-module";
 import {LoginTestComponent} from "./login-test/login-test.component";
 import {LoginFormTestComponent} from "./login-form-test/login-form-test.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {ResetPasswordComponent} from "./reset-password/reset-password.component";
+import {LoginRegisterImageComponent} from "./login-register-image/login-register-image.component";
 
 @NgModule({
   declarations: [
     TestComponentComponent,
     LoginTestComponent,
-    LoginFormTestComponent
+    LoginFormTestComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetPasswordComponent,
+    LoginRegisterImageComponent,
   ],
   imports: [CommonModule, RouterModule, FormsModule, MaterialModule, ReactiveFormsModule],
   exports: [
     TestComponentComponent,
     LoginTestComponent,
-    LoginFormTestComponent
+    LoginFormTestComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetPasswordComponent,
+    LoginRegisterImageComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
---
+++ src/app/components/login-register-image/login-register-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-register-image',
+  templateUrl: './login-register-image.component.html',
+  styleUrl: './login-register-image.component.css'
+})
+export class LoginRegisterImageComponent {
+
+}

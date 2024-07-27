f4c344f7f4992d0828ce52a6290e8fb9187ed6cb
daadii <dadi1@speed.at>
2024-07-27 21:24:30 +0200

    implemented first footer style version

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'footer-component',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  @Input()
+  footerDataPrivacy: string = 'Data Privacy'
+  @Input()
+  footerImprint: string = 'Imprint'
+  @Input()
+  imageAlt: string = 'logo'
+  @Input()
+  footerImageSrc: string = '/assets/logo_black-1500h.png'
+  @Input()
+  footerCopyrightInformation: string = '© 2024 JaLeHD, All Rights Reserved.'
+  @Input()
+  rootClassName: string = ''
 }

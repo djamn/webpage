d83f48e9a93807c0e0f4f07a821781075b98ee0f
daadii <dadi1@speed.at>
2024-07-27 12:29:37 +0200

    implemented style of login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UntypedFormControl, UntypedFormGroup} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {ConfigService} from "../../services/config.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'login-component',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  hidePassword = true;
+  loginForm!: UntypedFormGroup;
+
+  constructor(private authService: AuthService, private snackBar: MatSnackBar, private configService: ConfigService, private router: Router) {
+  }
+
+  ngOnInit() {
+     this.loginForm = new UntypedFormGroup({
+       usernameOrEmail: new UntypedFormControl('',[]),
+       password: new UntypedFormControl('',[]),
+     })
+  }
+
+  togglePasswordVisibility(event: MouseEvent): void {
+    event.stopPropagation();
+    this.hidePassword = !this.hidePassword;
+  }
+
+  async login() {
+
+  }
 
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ComponentsModule} from "../../components/components.module";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ComponentsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })

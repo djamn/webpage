5cd12d6240f7354df73c6f318a4c0b3da2b7c8b9
daadii <dadi1@speed.at>
2024-07-29 23:03:24 +0200

    translated footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,21 +1,27 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ConfigService} from "../../services/config.service";
 
 @Component({
   selector: 'footer-component',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   @Input()
   footerDataPrivacy: string = 'Data Privacy'
   @Input()
   footerImprint: string = 'Imprint'
   @Input()
-  imageAlt: string = 'logo'
-  @Input()
-  footerImageSrc: string = '/assets/logo_black-1500h.png'
-  @Input()
   footerCopyrightInformation: string = '© 2024 JaLeHD, All Rights Reserved.'
   @Input()
   rootClassName: string = ''
+  config: any;
+  currentYear: number = new Date().getFullYear();
+
+  constructor(private configService: ConfigService) {
+  }
+
+  ngOnInit() {
+    this.config = this.configService.getConfig();
+  }
 }

ea1a961272f4fbce503dcabe96b5908b67fe0d0a
daadii <dadi1@speed.at>
2024-08-12 23:35:28 +0200

    implemented first structure of main guestbook component

--- src/app/components/guestbook-main-component/guestbook-main-component.component.ts
+++ src/app/components/guestbook-main-component/guestbook-main-component.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './guestbook-main-component.component.css'
 })
 export class GuestbookMainComponent {
-
+    hasPermission() {
+      return true;
+    }
 }

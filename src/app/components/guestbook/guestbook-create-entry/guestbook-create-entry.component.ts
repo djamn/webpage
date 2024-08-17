0766111e364536c77bf00a3b9a7762aa62b8b27f
daadii <dadi1@speed.at>
2024-08-17 23:05:39 +0200

    disabled emojis

--- src/app/components/guestbook/guestbook-create-entry/guestbook-create-entry.component.ts
+++ src/app/components/guestbook/guestbook-create-entry/guestbook-create-entry.component.ts
@@ -5,7 +5,6 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {TranslateService} from "@ngx-translate/core";
 import {Snackbar} from "../../../utility/snackbar";
 import {isControlInvalid} from "../../../utility/form-utils";
-import {addWarning} from "@angular-devkit/build-angular/src/utils/webpack-diagnostics";
 import {GuestbookService} from "../../../services/guestbook.service";
 
 @Component({
@@ -22,19 +21,13 @@ export class GuestbookCreateEntryComponent implements OnInit {
     private router: Router,
     private translate: TranslateService,
     private snackbar: Snackbar,
-    private route : ActivatedRoute,
+    private route: ActivatedRoute,
     private guestbookService: GuestbookService,
     private fb: FormBuilder) {
   }
 
   ngOnInit() {
     this.config = this.configService.getConfig();
-
-    /*    this.createEntryForm = this.fb.group({
-          username: [{value: '', disabled: !this.config.GUESTBOOK_ENTRY_CREATION_POSSIBLE}, Validators.required],
-          entry_message: ['', Validators.required],
-          recaptcha: ['', Validators.required] // reCAPTCHA form control
-        });*/
 
     this.createEntryForm = new UntypedFormGroup({
       username: new UntypedFormControl({value: '', disabled: !this.config.GUESTBOOK_ENTRY_CREATION_POSSIBLE}, [
@@ -48,24 +41,28 @@ export class GuestbookCreateEntryComponent implements OnInit {
       recaptcha: new UntypedFormControl({value: '', disabled: !this.config.GUESTBOOK_ENTRY_CREATION_POSSIBLE}, [
         Validators.required,
       ]),
-      confirmation_checkbox: new UntypedFormControl({value: '', disabled: !this.config.GUESTBOOK_ENTRY_CREATION_POSSIBLE}, [
+      confirmation_checkbox: new UntypedFormControl({
+        value: '',
+        disabled: !this.config.GUESTBOOK_ENTRY_CREATION_POSSIBLE
+      }, [
         Validators.requiredTrue,
       ])
     })
   }
 
+  // TODO rework
   quillModules = {
     toolbar: [
-      [{ 'font': [] }],
+      [{'font': []}],
       ['bold', 'italic', 'underline'],
       ['code-block'],
-      [{ 'list': 'ordered' }],
-      [{ 'script': 'sub' }, { 'script': 'super' }],
-      [{ 'color': [] }, { 'background': [] }],
+      [{'list': 'ordered'}],
+      [{'script': 'sub'}, {'script': 'super'}],
+      [{'color': []}, {'background': []}],
       ['clean'],
       ['emoji']  // Add emoji to the toolbar
     ],
-    'emoji-toolbar': true,
+    'emoji-toolbar': false,
   };
 
   async createEntry() {
@@ -79,14 +76,14 @@ export class GuestbookCreateEntryComponent implements OnInit {
       Object.keys(this.createEntryForm.controls).forEach(field => {
         const control = this.createEntryForm.get(field);
         if (control) {
-          control.markAsTouched({ onlySelf: true });
+          control.markAsTouched({onlySelf: true});
         }
       });
       this.snackbar.showSnackbar(this.translate.instant('GUESTBOOK.CREATE.ERRORS.FIELDS_HAVE_ERRORS'), 'error-snackbar', this.config.SNACKBAR_ERROR_DURATION);
       return;
     }
 
-    if(!this.createEntryForm.value.recaptcha) {
+    if (!this.createEntryForm.value.recaptcha) {
       this.snackbar.showSnackbar(this.translate.instant('GUESTBOOK.CREATE.ERRORS.RECAPTCHA_EXPIRED'), 'error-snackbar', this.config.SNACKBAR_ERROR_DURATION);
     }
 
@@ -95,7 +92,7 @@ export class GuestbookCreateEntryComponent implements OnInit {
       await this.guestbookService.addEntry(entry.username.trim(), Date.now(), 'visible', true, entry.entry_message)
       this.snackbar.showSnackbar(this.translate.instant('GUESTBOOK.CREATE.ENTRY_CREATED_SUCCESSFUL'), 'success-snackbar', this.config.SNACKBAR_SUCCESS_DURATION)
       this.createEntryForm.reset();
-      await this.router.navigate(['../'], { relativeTo: this.route });
+      await this.router.navigate(['../'], {relativeTo: this.route});
     } catch (e) {
       console.error("Guestbook entry creation error:", e);
       this.snackbar.showSnackbar(this.translate.instant('GUESTBOOK.UNEXPECTED_ERROR'), 'error-snackbar', this.config.SNACKBAR_ERROR_DURATION);
@@ -109,7 +106,7 @@ export class GuestbookCreateEntryComponent implements OnInit {
   async abort(event: MouseEvent) {
     event.stopPropagation();
     this.createEntryForm.reset();
-    await this.router.navigate(['../'], { relativeTo: this.route });
+    await this.router.navigate(['../'], {relativeTo: this.route});
   }
 
   protected readonly isControlInvalid = isControlInvalid;

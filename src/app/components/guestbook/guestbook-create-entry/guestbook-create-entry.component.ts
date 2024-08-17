28e7823dc1c93777194af112797860025f2d62f1
daadii <dadi1@speed.at>
2024-08-17 23:17:40 +0200

    added required for checkbox

--- src/app/components/guestbook/guestbook-create-entry/guestbook-create-entry.component.ts
+++ src/app/components/guestbook/guestbook-create-entry/guestbook-create-entry.component.ts
@@ -66,6 +66,7 @@ export class GuestbookCreateEntryComponent implements OnInit {
   };
 
   async createEntry() {
+    console.log(this.createEntryForm.get('confirmation_checkbox'))
     if (!this.config.GUESTBOOK_ENTRY_CREATION_POSSIBLE) {
       this.snackbar.showSnackbar(this.translate.instant('GUESTBOOK.CREATE.ERRORS.NO_CREATION_POSSIBLE_HINT'), 'error-snackbar', this.config.SNACKBAR_ERROR_DURATION);
       return;

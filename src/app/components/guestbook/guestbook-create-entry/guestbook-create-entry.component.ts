f877eee67462e914145a3ed0107916b964d498db
daadii <dadi1@speed.at>
2024-08-27 22:30:28 +0200

    small fix

--- src/app/components/guestbook/guestbook-create-entry/guestbook-create-entry.component.ts
+++ src/app/components/guestbook/guestbook-create-entry/guestbook-create-entry.component.ts
@@ -104,7 +104,7 @@ export class GuestbookCreateEntryComponent implements OnInit {
 
       if (this.isEditMode) {
         await this.guestbookService.updateEntry(this.entryId!, trimmedUsername, Date.now(), false, formEntry.entry_message);
-        this.snackbar.showSnackbar(this.translate.instant('GUESTBOOK.CREATE.ENTRY_CREATED_SUCCESSFUL'), 'success-snackbar', this.config.SNACKBAR_SUCCESS_DURATION)
+        this.snackbar.showSnackbar(this.translate.instant('GUESTBOOK.CREATE.ENTRY_UPDATED_SUCCESSFUL'), 'success-snackbar', this.config.SNACKBAR_SUCCESS_DURATION)
       } else {
         await this.guestbookService.addEntry(trimmedUsername, Date.now(), 'visible', true, formEntry.entry_message);
         this.snackbar.showSnackbar(this.translate.instant('GUESTBOOK.CREATE.ENTRY_CREATED_SUCCESSFUL'), 'success-snackbar', this.config.SNACKBAR_SUCCESS_DURATION)

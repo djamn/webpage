4a2eeaa36ac553a021d18ac1551b4a32c1ca1031
daadii <dadi1@speed.at>
2024-09-02 23:30:34 +0200

    added classes for main page

---
+++ src/app/components/mainpage/mainpage-main-component/mainpage-main-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mainpage-main-component',
+  templateUrl: './mainpage-main-component.component.html',
+  styleUrl: './mainpage-main-component.component.css'
+})
+export class MainpageMainComponent {
+
+}

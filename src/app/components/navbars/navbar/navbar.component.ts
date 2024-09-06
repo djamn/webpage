4150249362f85091501b9c3c5ea1dc3314f7d8ca
daadii <dadi1@speed.at>
2024-09-06 18:17:24 +0200

    added first layout for translation header

--- src/app/components/navbars/navbar/navbar.component.ts
+++ src/app/components/navbars/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core'
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-navbar',
@@ -18,7 +19,15 @@ export class Navbar {
   navbarLinkBikeStations: string = 'Bike Stations'
   @Input()
   navbarButtonRegister: string = 'Register'
+  selectedLang: string = 'de';
+  selectedCityName: string = 'Vilnius'
 
-  constructor() {
+  cities3 = [
+    {id: 1, name: 'Vilnius', avatar: '//www.gravatar.com/avatar/b0d8c6e5ea589e6fc3d3e08afb1873bb?d=retro&r=g&s=30 2x'},
+    {id: 2, name: 'Kaunas', avatar: '//www.gravatar.com/avatar/ddac2aa63ce82315b513be9dc93336e5?d=retro&r=g&s=15'},
+    {id: 3, name: 'Pavilnys', avatar: '//www.gravatar.com/avatar/6acb7abf486516ab7fb0a6efa372042b?d=retro&r=g&s=15'}
+  ];
+
+  constructor(public translate: TranslateService) {
   }
 }

7eb16eb7d173d47d90821c4a89ddbc7cd012fecf
daadii <dadi1@speed.at>
2024-09-12 21:18:44 +0200

    migrated button general to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   darkMode: 'selector',
   // darkMode: ['selector', '[data-mode="dark"]'],
-  // darkMode: ['variant', '&:not(.light *)'],
+  // darkMode: ['variant', '&:not(.light *)'],#
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'btn-1': '0 1px 0 rgba(27,31,35,0.1)'
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),

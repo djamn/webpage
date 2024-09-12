dd809a65798e56ee3b9b8ecbb7fcf8dd098aacc8
daadii <dadi1@speed.at>
2024-09-12 20:17:00 +0200

    beginning to migrate login component to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
+  // darkMode: ['selector', '[data-mode="dark"]'],
+  // darkMode: ['variant', '&:not(.light *)'],
   content: ["./src/**/*.{html,ts}",],
   theme: {
     extend: {},

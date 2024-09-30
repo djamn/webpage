62954b7f54dd2627e94dc05dfc85b3f5ce22c94f
daadii <dadi1@speed.at>
2024-09-30 22:13:54 +0200

    small adjustments and fixes

--- src/app/components/mainpage-introduction-component/mainpage-introduction-component.component.ts
+++ src/app/components/mainpage-introduction-component/mainpage-introduction-component.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import {ConfigService} from "../../services/config.service";
 
 @Component({
@@ -6,24 +6,34 @@ import {ConfigService} from "../../services/config.service";
   templateUrl: './mainpage-introduction-component.component.html',
   styleUrl: './mainpage-introduction-component.component.css'
 })
-export class MainPageIntroductionComponent implements OnInit {
+export class MainPageIntroductionComponent implements OnInit, OnDestroy {
   config: any;
-  keywords = ['Java ', 'AngularJS ', 'Typescript ']; // Keywords to type
+  keywords: string[] = ['Java ', 'AngularJS ', 'Typescript ']; // Space is needed, dont know why
   // colors = ['text-red-500', 'text-blue-500', 'text-green-500']; // Tailwind classes for colors
-  colors = ['blue', 'green', 'red'];
-  currentWordIndex = 0;
-  displayText = '';
-  isDeleting = false;
-  typingSpeed = 150;
-  deletingSpeed = 100;
-  currentColor = this.colors[0];
+  colors: string[] = ['blue', 'green', 'red'];
+  currentWordIndex: number = 0;
+  displayText: string = '';
+  isDeleting: boolean = false;
+  typingSpeed: number = 150;
+  deletingSpeed: number = 100;
+  currentColor: string = this.colors[0];
+  active: boolean = false;
+
+  constructor(private configService: ConfigService, private cdr: ChangeDetectorRef, private ngZone: NgZone) {
+    this.config = this.configService.getConfig();
+  }
 
   ngOnInit() {
+    this.active = true;
     this.ngZone.runOutsideAngular(() => this.startTyping());
-    this.startTyping();
+  }
+
+  ngOnDestroy() {
+    this.active = false;
   }
 
   startTyping() {
+    if (!this.active) return;
     const currentWord = this.keywords[this.currentWordIndex];
     const currentLength = this.displayText.length;
 
@@ -32,8 +42,7 @@ export class MainPageIntroductionComponent implements OnInit {
       if (this.displayText === '') {
         this.isDeleting = false;
         this.currentWordIndex = (this.currentWordIndex + 1) % this.keywords.length;
-        this.currentColor = this.colors[this.currentWordIndex]; // Change color
-        console.log(this.currentColor);
+        this.currentColor = this.colors[this.currentWordIndex];
       }
     } else {
       this.displayText = currentWord.substring(0, currentLength + 1);
@@ -46,14 +55,8 @@ export class MainPageIntroductionComponent implements OnInit {
       }
     }
 
+    this.cdr.detectChanges(); // Needed otherwise it will not be executed
     const delay = this.isDeleting ? this.deletingSpeed : this.typingSpeed;
-
-    this.cdr.detectChanges();
-
     setTimeout(() => this.ngZone.runOutsideAngular(() => this.startTyping()), delay);
-  }
-
-  constructor(private configService: ConfigService, private cdr : ChangeDetectorRef, private ngZone: NgZone) {
-    this.config = this.configService.getConfig();
   }
 }

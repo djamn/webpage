fb900ee4671966df935bcfe53e5a7a94a4dfc454
daadii <dadi1@speed.at>
2024-10-06 21:40:13 +0200

    implemented cat checker data handling

--- src/app/components/cat-check-component/cat-check-component.component.ts
+++ src/app/components/cat-check-component/cat-check-component.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {faCat} from '@fortawesome/free-solid-svg-icons'
+import {Snackbar} from "../../utility/snackbar";
+import {CheckerService} from "./checker.service";
+import {CatChecker} from "./checker.type";
 
 @Component({
   selector: 'cat-check-component',
@@ -10,5 +13,84 @@ import {faCat} from '@fortawesome/free-solid-svg-icons'
   styleUrl: './cat-check-component.component.css'
 })
 export class CatCheckComponent {
-    faCat = faCat
+  faCat = faCat
+  feedingSessions: CatChecker[] = [];
+  feedingCount: number = 0;
+  feedsToday: number = 0;
+  feedsYesterday: number = 0;
+  lastFeedingSession: string = 'Unbekannt'
+
+  constructor(private snackbar: Snackbar, private checker: CheckerService) {
+    this.fetchFeedingSessions();
+  }
+
+  fetchFeedingSessions(): void {
+    this.checker.getFeedingSessions().subscribe({
+      next: (data) => {
+        this.feedingSessions = data;
+        this.feedingCount = this.feedingSessions.length;
+        this.getLastFeedingSession();
+        this.fetchTodaysFeedingSessions();
+        this.fetchYesterdaysFeedingSessions();
+        this.fetchDailyAverage();
+      },
+      error: (err) => {
+        console.error('Error fetching feeding sessions:', err);
+      }
+    })
+  }
+
+  getLastFeedingSession() {
+    this.lastFeedingSession = this.processTimestamp(this.feedingSessions[0].timestamp)
+  }
+
+  // TODO utility function
+  processTimestamp(timestamp: any) {
+    const date = new Date(timestamp)
+    const formattedDate = date.toLocaleDateString();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${formattedDate} (${hours}:${minutes})`
+  }
+
+  fetchTodaysFeedingSessions() {
+    const sessionsToday = this.feedingSessions.filter(session => {
+      const sessionDate = new Date(session.timestamp);
+      return sessionDate >= this.getMidnightDate();
+    });
+
+    this.feedsToday = sessionsToday.length;
+  }
+
+  fetchYesterdaysFeedingSessions() {
+    const todayDate = this.getMidnightDate();
+
+    const startOfYesterday = new Date(todayDate);
+    startOfYesterday.setDate(todayDate.getDate() - 1); // Move one day back
+
+    const endOfYesterday = new Date(todayDate);
+    endOfYesterday.setMilliseconds(-1); // One millisecond before today's midnight
+
+    const sessionsYesterday = this.feedingSessions.filter(session => {
+      const sessionDate = new Date(session.timestamp);
+      return sessionDate >= startOfYesterday && sessionDate <= endOfYesterday;
+    });
+
+    this.feedsYesterday = sessionsYesterday.length;
+  }
+
+  getMidnightDate(): Date {
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+    return startOfDay;
+  }
+
+  fetchDailyAverage() {
+
+  }
+
+
+  feedCat() {
+
+  }
 }

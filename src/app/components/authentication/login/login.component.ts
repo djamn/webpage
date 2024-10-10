8fa6f41674add421f9bbbf4240b86379b7a052d5
djamn <david@jamnig.net>
2024-10-10 12:11:22 +0200

    reworked login icons

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -6,6 +6,7 @@ import {Router} from "@angular/router";
 import {Snackbar} from "../../../utility/snackbar";
 import {TranslateService} from "@ngx-translate/core";
 import {isControlInvalid} from "../../../utility/form-utils";
+import {faEnvelope, faEye, faEyeSlash, faLock, faPlus} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'login-component',
@@ -83,4 +84,9 @@ export class LoginComponent implements OnInit {
   }
 
   protected readonly isControlInvalid = isControlInvalid;
+    protected readonly faPlus = faPlus;
+  protected readonly faEnvelope = faEnvelope;
+  protected readonly faLock = faLock;
+  protected readonly faEye = faEye;
+  protected readonly faEyeSlash = faEyeSlash;
 }

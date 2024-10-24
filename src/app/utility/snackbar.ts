b72d4fb46ecd5a996b22fc35e1d4e93c82071fda
djamn <david@jamnig.net>
2024-10-24 13:10:57 +0200

    added email snackbar

--- src/app/utility/snackbar.ts
+++ src/app/utility/snackbar.ts
@@ -1,12 +1,17 @@
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {Injectable} from "@angular/core";
 import {TranslateService} from "@ngx-translate/core";
+import {AuthService} from "../services/auth.service";
+import {ConfigService} from "../services/config.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class Snackbar {
-  constructor(private snackBar: MatSnackBar, private translate: TranslateService) {
+  config: any;
+
+  constructor(private snackBar: MatSnackBar, private translate: TranslateService, private configService: ConfigService) {
+    this.config = this.configService.getConfig();
   }
 
   showSnackbar(message: string, styleClass: string, durationMs: number) {
@@ -15,6 +20,15 @@ export class Snackbar {
       horizontalPosition: 'center',
       panelClass: [styleClass],
       duration: durationMs
+    });
+  }
+
+  showEmailSnackbar() {
+    return this.snackBar.open(this.translate.instant('LOGIN.ERRORS.EMAIL_NOT_VERIFIED'), this.translate.instant('SNACKBAR.RESEND_MAIL_BUTTON'), {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+      panelClass: ['warning-snackbar'],
+      duration: this.config.SNACKBAR_ERROR_DURATION
     });
   }
 }
